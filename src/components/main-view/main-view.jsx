8a0e8b206008cd2852315a4b06074d20be6981d1
agusNiko <73887894+agusNiko@users.noreply.github.com>
2021-11-11 18:16:47 +0100

    create MainView component

---
+++ src/components/main-view/main-view.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+class MainView extends React.Component {
+    constructor() {
+      super();
+      this.state = {color: "red"};
+    }
+    render() {
+
+        return <>
+            <h1>21DAYS fitness APP!</h1>
+            <h2>--- Comming soon ---</h2>
+        </>
+        
+    }
+  }
+
+  export default MainView;
