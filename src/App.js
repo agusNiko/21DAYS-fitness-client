83a1e0576a255c89bc0e5b46302e20818bf00fa9
agusNiko <73887894+agusNiko@users.noreply.github.com>
2021-12-06 20:35:04 +0100

    remove the case style

--- src/App.js
+++ src/App.js
@@ -4,17 +4,7 @@ import MainView from "./components/main-view/Main-view";
 function App() {
   return (
     <div className="App">
-      <div className="Case">
-        <div className="Case-top "></div>
-        <div className="Case-middle">
-          <div className="Case-sides"></div>
-          <MainView />
-          <div className="Case-sides"></div>
-        </div>
-        <div className="Case-bottom">
-          <div className="Case-bottom_center"></div>
-        </div>
-      </div>
+      <MainView />
     </div>
   );
 }
