bd5c755a8c622de721a654c4d568f962078d2899
agusNiko <73887894+agusNiko@users.noreply.github.com>
2021-12-08 13:37:32 +0100

    add about component to landing page component

--- src/components/landing-page/landing-page.jsx
+++ src/components/landing-page/landing-page.jsx
@@ -3,31 +3,36 @@ import "./landing-page.css";
 
 import React from "react";
 import Img from "./img";
+import { height } from "@mui/system";
+import About from "./about";
 
 const LandingPage = (props) => {
   return (
-    <div className="landing-page__box">
-      <div className="landing-page__top">
-        <div className="circle">
-          <div className="center-circle"></div>
+    <div>
+      <div className="landing-page__box">
+        <div className="landing-page__top">
+          <div className="circle">
+            <div className="center-circle"></div>
+          </div>
+          <div>
+            <p>Homeworkouts on your terms</p>
+          </div>
         </div>
-        <div>
-          <p>Homeworkouts on your terms</p>
+        <div className="landing-page__middle">
+          <Img></Img>
+        </div>
+        <div className="landing-page__bottom">
+          <button onClick={() => props.toLogin()}>Sign In</button>
+          <button onClick={() => console.log("connect with google")}>
+            <Google className="google-logo" /> Sign in with google
+          </button>
+          <div onClick={() => props.toRegister()} style={{ cursor: "pointer" }}>
+            <p>Are you new? Register here!</p>
+          </div>
+          <KeyboardArrowDown className="down-arrow__icon"></KeyboardArrowDown>
         </div>
       </div>
-      <div className="landing-page__middle">
-        <Img></Img>
-      </div>
-      <div className="landing-page__bottom">
-        <button onClick={() => props.toLogin()}>Sign In</button>
-        <button onClick={() => console.log("connect with google")}>
-          <Google className="google-logo" /> Sign in with google
-        </button>
-        <div onClick={() => props.toRegister()} style={{ cursor: "pointer" }}>
-          <p>Are you new? Register here!</p>
-        </div>
-        <KeyboardArrowDown className="down-arrow__icon"></KeyboardArrowDown>
-      </div>
+      <About></About>
     </div>
   );
 };
