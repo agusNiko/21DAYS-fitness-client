c12e6fe33d2dd8d0c93b405e2105eaccca3133cb
agusNiko <73887894+agusNiko@users.noreply.github.com>
2021-12-09 15:04:10 +0100

    modify burger header with burger menu icon

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,34 +1,17 @@
 import React from "react";
 import "./Header.css";
-import Navbar from "react-bootstrap/Navbar";
-import Nav from "react-bootstrap/Nav";
-import Container from "react-bootstrap/Container";
-import NavDropdown from "react-bootstrap/NavDropdown";
+import { Menu } from "@mui/icons-material";
+import "./Header.css";
 
-const header = () => {
+const Header = () => {
   return (
-    <Navbar bg="light" expand="lg">
-      <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <div>
+      <div>
+        <h1>21 Days</h1>
+      </div>
+      <Menu style={{ color: "black" }}></Menu>
+    </div>
   );
 };
 
-export default header;
+export default Header;
